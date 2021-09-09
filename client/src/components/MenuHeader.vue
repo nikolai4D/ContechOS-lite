d26f6219cbab6629fdc2e846f0779c879f0826ef
Davide <cerato.davide2002@gmail.com>
2021-09-09 15:56:53 +0200

    fixed menu toggle

--- client/src/components/MenuHeader.vue
+++ client/src/components/MenuHeader.vue
@@ -3,12 +3,12 @@
         <i
         class="fas fa-caret-right col-sm-2"
         v-if="toggleMenu"
-        @click="toggleMenu = false"
+        @click="menuToggle(false)"
         ></i>
         <i
         class="fas fa-caret-down col-sm-2"
         v-if="!toggleMenu"
-        @click="toggleMenu = true"
+        @click="menuToggle(true)"
         ></i>
         <h3 class="col-sm-8">{{menuName}}</h3>
         <i class="fas fa-times col-sm-2" @click="closeElement($event)"></i>
@@ -36,7 +36,7 @@ export default defineComponent({
     },
     mounted() {
         this.enableDrag();
-        console.log(this.$parent.$parent.$parent)
+        // console.log(document.getElementById("header").parentElement.parentElement)
     },
     data() {
         return {
@@ -45,51 +45,55 @@ export default defineComponent({
         };
     },
     methods: {
-        closeElement(event) {
-            this.parent = event.target.parentElement.parentElement.parentElement
-            this.parent.classList.remove("show");
-            this.parent.style.display = "none";
-            this.toggleMenu = true;
-        },
-    enableDrag() {
-      let x = 0;
-      let y = 0;
+      closeElement(event) {
+          this.parent = event.target.parentElement.parentElement.parentElement
+          this.parent.classList.remove("show");
+          this.parent.style.display = "none";
+          this.toggleMenu = true;
+      },
+      menuToggle(newValue) {
+        this.toggleMenu = newValue
+        this.$emit('menuToggle', newValue)
+      },
+      enableDrag() {
+        let x = 0;
+        let y = 0;
 
-      let isDragging = false;
+        let isDragging = false;
 
-      const draggableElement = document.getElementById("header");
+        const draggableElement = document.getElementById("header");
 
-      draggableElement.addEventListener("mousedown", () => {
-        function update() {
-          if (isDragging) {
-            requestAnimationFrame(update);
+        draggableElement.addEventListener("mousedown", () => {
+          function update() {
+            if (isDragging) {
+              requestAnimationFrame(update);
+            }
+
+            document.getElementById(
+              "addNode"
+            ).style.transform = `translate(${x}px, ${y}px)`;
           }
 
-          document.getElementById(
-            "addNode"
-          ).style.transform = `translate(${x}px, ${y}px)`;
-        }
+          isDragging = true;
 
-        isDragging = true;
+          requestAnimationFrame(update);
 
-        requestAnimationFrame(update);
+        })
 
-      })
+        document.addEventListener("mousemove", (event) => {
+          if (!isDragging) {
+            return;
+          }
 
-      document.addEventListener("mousemove", (event) => {
-        if (!isDragging) {
-          return;
-        }
+          x = event.clientX;
+          y = event.clientY;
 
-        x = event.clientX;
-        y = event.clientY;
+        })
 
-      })
-
-      document.addEventListener("mouseup", () => {
-        isDragging = false;
-      })
-    }
+        document.addEventListener("mouseup", () => {
+          isDragging = false;
+        })
+      }
     },
 })
 </script>
