72db161c38f33e060cdd1386668c98600305b9a3
Davide <cerato.davide2002@gmail.com>
2021-09-13 09:50:30 +0200

    added edit node

--- client/src/components/EditNode.vue
+++ client/src/components/EditNode.vue
@@ -9,10 +9,10 @@
       <ul class="list-unstyled components p-3 pb-0" v-if="toggleMenu">
         <Labels :lbl="labels" @labelsChanged="changeLabels($event)" />
         <Attributes
-          :attr="attributes"
-          @attributesChanged="changeAttributes($event)"
+          :attr="properties"
+          @attributesChanged="changeProperties($event)"
         />
-        <button type="submit" class="form form-control btn btn-primary mt-3">
+        <button type="submit" class="form form-control btn btn-primary mt-3" @click="editNode">
           Edit Node
         </button>
       </ul>
@@ -37,6 +37,8 @@ import { defineComponent } from "vue";
 import Attributes from "./Attributes.vue";
 import Labels from "./Labels.vue";
 import MenuHeader from "./MenuHeader.vue";
+import { useMutation, useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
 
 export default defineComponent({
   name: "EditNode",
@@ -46,9 +48,27 @@ export default defineComponent({
       currentPos: { x: null, y: null },
       isMouseDown: false,
       toggleMenu: true,
-      labels: ["User", "Admin"],
-      attributes: { name: "luke" },
+      labels: [],
+      properties: {},
+      activeNodeId: ""
     };
+  },
+  props:{
+    nodeId: String,
+    labelsProps: [],
+    propertiesProps: {},
+  },
+  watch: {
+    labelsProps(newValue, oldValue){
+      this.labels = newValue
+    },
+    propertiesProps(newValue, oldValue){
+      var cleanProperties = this.removeUnnecessaryProperties(newValue)
+      this.properties = cleanProperties
+    },
+    nodeId(newValue, oldValue){
+      this.activeNodeId = newValue
+    }
   },
   components: {
     Attributes,
@@ -56,12 +76,51 @@ export default defineComponent({
     MenuHeader,
   },
   methods: {
-    changeAttributes(event: any) {
-      this.attributes = event;
+    changeProperties(event: any) {
+      this.properties = event;
     },
     changeLabels(event: any) {
       this.labels = event;
     },
+    removeUnnecessaryProperties(properties:any) {
+      delete properties["createdAt"];
+      delete properties["id"];
+      delete properties["updatedAt"];
+      return properties
+    },
+    editNode() {
+      var properties = this.properties
+      var labels = this.labels
+      var id = this.activeNodeId
+
+      const { mutate, onDone, onError } = useMutation(gql`
+        mutation ($labels: [String!]!, $properties: JSONObject, $id:String!){
+          updateNode(
+            updateNodeInput: {
+              labels:$labels
+              properties:$properties
+            }
+            id:$id
+          ) {
+            id
+            labels
+            properties
+          }
+        }
+      `);
+
+      mutate({ properties:properties, labels:labels, id:id });
+
+      onDone((result) => {
+        this.$el.classList.remove("show");
+        this.$el.style.display = "none";
+      });
+
+      onError((result) => {
+        console.log(result.graphQLErrors[0].extensions?.response.message);
+        alert(result.graphQLErrors[0].extensions?.response.message);
+      });
+    }
   },
 });
 </script>
