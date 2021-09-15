f08e6e3eb9f65560a6822016376a24aebf8a6f75
Alex Sandri <me@alexsandri.com>
2021-09-15 11:29:38 +0200

    Start fixing graph jumpiness on updates

--- client/src/views/AppPage.vue
+++ client/src/views/AppPage.vue
@@ -85,6 +85,18 @@ export default defineComponent({
       properties: {},
       relationshipName: "",
       targetElementId: "",
+      simulation: null,
+      nodes: [],
+      relationships: [],
+    } as {
+      activeElementId: string;
+      labels: string[];
+      properties: any;
+      relationshipName: string;
+      targetElementId: string;
+      simulation: any;
+      nodes: any[],
+      relationships: any[],
     };
   },
   components: {
@@ -206,10 +218,13 @@ export default defineComponent({
         }
       `);
 
-      mutate({ id: id });
+      mutate({ id });
 
       onDone((result) => {
-        this.getAllUsers();
+        this.update({
+          nodes: this.nodes.filter((node) => node.id !== id),
+          relationships: this.relationships,
+        });
       });
 
       onError((result) => {
@@ -258,7 +273,10 @@ export default defineComponent({
       mutate({ id });
 
       onDone((result) => {
-        this.getAllUsers();
+        this.update({
+          nodes: this.nodes,
+          relationships: this.relationships.filter((relationship) => relationship.id !== id),
+        });
       });
 
       onError((result) => {
@@ -344,157 +362,64 @@ export default defineComponent({
       });
 
       onDone((result) => {
-        const { nodes, relationships } = result.data;
+        this.update(result.data);
+      });
+    },
+    async update({ nodes, relationships }: any) {
+      const svg = document.querySelector("svg") as SVGElement;
+      svg.innerHTML = "";
+      svg.setAttribute("viewBox", `0 0 ${svg.clientWidth} ${svg.clientHeight}`);
 
-        const links = relationships.map((relationship: any) => {
-          return {
-            id: relationship.id,
-            name: relationship.name,
-            source: nodes.find(
-              (node: any) => node.id === relationship.source.id
-            ),
-            target: nodes.find(
-              (node: any) => node.id === relationship.target.id
-            ),
-          };
-        });
+      const links = relationships.map((relationship: any) => {
+        return {
+          id: relationship.id,
+          name: relationship.name,
+          source: nodes.find((node: any) => node.id === relationship.source.id),
+          target: nodes.find((node: any) => node.id === relationship.target.id),
+        };
+      });
 
-        const svg = document.querySelector("svg") as SVGElement;
-        svg.innerHTML = "";
-        svg.setAttribute(
-          "viewBox",
-          `0 0 ${svg.clientWidth} ${svg.clientHeight}`
-        );
+      this.simulation = d3
+        .forceSimulation(nodes)
+        .force(
+          "link",
+          d3
+            .forceLink(links)
+            .distance(200)
+            .id((data: any) => data.id)
+        )
+        .force("charge", d3.forceManyBody())
+        .force(
+          "collide",
+          d3
+            .forceCollide()
+            .radius(100)
+            .iterations(2)
+        )
+        .force(
+          "center",
+          d3.forceCenter(svg.clientWidth / 2, svg.clientHeight / 2)
+        )
+        .on("tick", () => {
+          linksSelection
+            .attr("x1", (data: any) => data.source.x)
+            .attr("y1", (data: any) => data.source.y)
+            .attr("x2", (data: any) => data.target.x)
+            .attr("y2", (data: any) => data.target.y);
 
-        const simulation = d3
-          .forceSimulation(nodes)
-          .force(
-            "link",
-            d3
-              .forceLink(links)
-              .distance(200)
-              .id((data: any) => data.id)
-          )
-          .force("charge", d3.forceManyBody())
-          .force(
-            "center",
-            d3.forceCenter(svg.clientWidth / 2, svg.clientHeight / 2)
-          )
-          .on("tick", () => {
-            linksSelection
-              .attr("x1", (data: any) => data.source.x)
-              .attr("y1", (data: any) => data.source.y)
-              .attr("x2", (data: any) => data.target.x)
-              .attr("y2", (data: any) => data.target.y);
+          nodesSelection
+            .attr("cx", (data: any) => data.x)
+            .attr("cy", (data: any) => data.y);
 
-            nodesSelection
-              .attr("cx", (data: any) => data.x)
-              .attr("cy", (data: any) => data.y);
-
-            document.querySelectorAll("circle").forEach((circle) => {
-              const text = circle.nextElementSibling!;
-
-              text.setAttribute("x", circle.getAttribute("cx")!);
-              text.setAttribute("y", circle.getAttribute("cy")!);
-            });
-
-            document.querySelectorAll("line").forEach((line) => {
-              const text = line.nextElementSibling!;
-              text.setAttribute(
-                "x",
-                (
-                  (line.x2.baseVal.value - line.x1.baseVal.value) / 2 +
-                  line.x1.baseVal.value
-                ).toString()
-              );
-              text.setAttribute(
-                "y",
-                (
-                  (line.y2.baseVal.value - line.y1.baseVal.value) / 2 +
-                  line.y1.baseVal.value
-                ).toString()
-              );
-            });
-          });
-
-        const linksSelection = d3
-          .select("svg")
-          .append("g")
-          .attr("stroke", "#999")
-          .attr("stroke-opacity", 0.6)
-          .selectAll("line")
-          .data(links)
-          .join("line")
-          .attr("marker-end", "url(#arrowhead)")
-          .attr("id", (data: any) => data.id);
-
-        const arrowHeads = d3
-          .select("svg")
-          .append("g")
-          .selectAll("marker")
-          .data(links)
-          .join("marker")
-          .attr("id", "arrowhead")
-          .attr("markerUnits", "strokeWidth")
-          .attr("markerWidth", 12)
-          .attr("markerHeight", 12)
-          .attr("viewBox", "0 0 12 12")
-          .attr("refX", 6)
-          .attr("refY", 6)
-          .attr("orient", "auto")
-          .append("path")
-          .attr("d", "M2,2 L10,6 L2,10 L6,6 L2,2")
-          .attr("style", "fill: #f00;");
-
-        const nodesSelection = d3
-          .select("svg")
-          .append("g")
-          .selectAll("circle")
-          .data(nodes)
-          .join("circle")
-          .attr("r", 40)
-          .attr("fill", (data: any) => stringToColor(data.labels[0]))
-          .attr("stroke", "#ffffff")
-          .attr("stroke-width", 1.5)
-          .attr("id", (data: any) => data.id);
-
-        nodesSelection
-          .append("title")
-          .text((data: any) => data.properties.name ?? data.labels[0]);
-        linksSelection.append("title").text((data: any) => data.name);
-
-        nodesSelection.call(drag(simulation));
-
-        function drag(simulation: any): any {
           document.querySelectorAll("circle").forEach((circle) => {
-            const text = document.createElementNS(
-              "http://www.w3.org/2000/svg",
-              "text"
-            );
+            const text = circle.nextElementSibling!;
 
-            text.setAttribute("fill", "#ffffff");
-            text.setAttribute("font-size", "14px");
-            text.setAttribute("text-anchor", "middle");
-            text.setAttribute("pointer-events", "none");
-            text.setAttribute("alignment-baseline", "middle");
-            text.setAttribute("style", "user-select: none;");
-
-            text.textContent = circle.querySelector("title")!.textContent!;
-
-            circle.insertAdjacentElement("afterend", text);
+            text.setAttribute("x", circle.getAttribute("cx")!);
+            text.setAttribute("y", circle.getAttribute("cy")!);
           });
-          document.querySelectorAll("line").forEach((line: any) => {
-            const text = document.createElementNS(
-              "http://www.w3.org/2000/svg",
-              "text"
-            );
 
-            text.setAttribute("fill", "black");
-            text.setAttribute("font-size", "10px");
-            text.setAttribute("text-anchor", "middle");
-            text.setAttribute("alignment-baseline", "middle");
-            text.setAttribute("style", "user-select: none;");
-            text.setAttribute("id", line.id);
+          document.querySelectorAll("line").forEach((line) => {
+            const text = line.nextElementSibling!;
             text.setAttribute(
               "x",
               (
@@ -509,44 +434,144 @@ export default defineComponent({
                 line.y1.baseVal.value
               ).toString()
             );
-
-            text.textContent = line.querySelector("title").textContent;
-
-            line.insertAdjacentElement("afterend", text);
           });
+        });
 
-          function dragstarted(event: any) {
-            // first hide all open menus
-            document
-              .querySelectorAll<HTMLElement>(".context-menu")
-              .forEach((el) => {
-                el.classList.remove("show");
-                el.style.display = "none";
-              });
+      this.simulation.stop();
 
-            if (!event.active) simulation.alphaTarget(0.3).restart();
-            event.subject.fx = event.subject.x;
-            event.subject.fy = event.subject.y;
-          }
+      const linksSelection = d3
+        .select("svg")
+        .append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("marker-end", "url(#arrowhead)")
+        .attr("id", (data: any) => data.id);
 
-          function dragged(event: any) {
-            event.subject.fx = event.x;
-            event.subject.fy = event.y;
-          }
+      /*
+      const arrowHeads = d3
+        .select("svg")
+        .append("g")
+        .selectAll("marker")
+        .data(links)
+        .join("marker")
+        .attr("id", "arrowhead")
+        .attr("markerUnits", "strokeWidth")
+        .attr("markerWidth", 12)
+        .attr("markerHeight", 12)
+        .attr("viewBox", "0 0 12 12")
+        .attr("refX", 6)
+        .attr("refY", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M2,2 L10,6 L2,10 L6,6 L2,2")
+        .attr("style", "fill: #f00;");
+        */
 
-          function dragended(event: any) {
-            if (!event.active) simulation.alphaTarget(0);
-            event.subject.fx = null;
-            event.subject.fy = null;
-          }
+      const nodesSelection = d3
+        .select("svg")
+        .append("g")
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 40)
+        .attr("fill", (data: any) => stringToColor(data.labels[0]))
+        .attr("stroke", "#ffffff")
+        .attr("stroke-width", 1.5)
+        .attr("id", (data: any) => data.id);
 
-          return d3
-            .drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended);
+      nodesSelection
+        .append("title")
+        .text((data: any) => data.properties.name ?? data.labels[0]);
+      linksSelection.append("title").text((data: any) => data.name);
+
+      nodesSelection.call(drag(this.simulation));
+
+      this.simulation!.restart();
+
+      function drag(simulation: any): any {
+        document.querySelectorAll("circle").forEach((circle) => {
+          const text = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "text"
+          );
+
+          text.setAttribute("fill", "#ffffff");
+          text.setAttribute("font-size", "14px");
+          text.setAttribute("text-anchor", "middle");
+          text.setAttribute("pointer-events", "none");
+          text.setAttribute("alignment-baseline", "middle");
+          text.setAttribute("style", "user-select: none;");
+
+          text.textContent = circle.querySelector("title")!.textContent!;
+
+          circle.insertAdjacentElement("afterend", text);
+        });
+        document.querySelectorAll("line").forEach((line: any) => {
+          const text = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "text"
+          );
+
+          text.setAttribute("fill", "black");
+          text.setAttribute("font-size", "10px");
+          text.setAttribute("text-anchor", "middle");
+          text.setAttribute("alignment-baseline", "middle");
+          text.setAttribute("style", "user-select: none;");
+          text.setAttribute("id", line.id);
+          text.setAttribute(
+            "x",
+            (
+              (line.x2.baseVal.value - line.x1.baseVal.value) / 2 +
+              line.x1.baseVal.value
+            ).toString()
+          );
+          text.setAttribute(
+            "y",
+            (
+              (line.y2.baseVal.value - line.y1.baseVal.value) / 2 +
+              line.y1.baseVal.value
+            ).toString()
+          );
+
+          text.textContent = line.querySelector("title").textContent;
+
+          line.insertAdjacentElement("afterend", text);
+        });
+
+        function dragstarted(event: any) {
+          // first hide all open menus
+          document
+            .querySelectorAll<HTMLElement>(".context-menu")
+            .forEach((el) => {
+              el.classList.remove("show");
+              el.style.display = "none";
+            });
+
+          if (!event.active) simulation.alphaTarget(0.3).restart();
+          event.subject.fx = event.subject.x;
+          event.subject.fy = event.subject.y;
         }
-      });
+
+        function dragged(event: any) {
+          event.subject.fx = event.x;
+          event.subject.fy = event.y;
+        }
+
+        function dragended(event: any) {
+          if (!event.active) simulation.alphaTarget(0);
+          event.subject.fx = null;
+          event.subject.fy = null;
+        }
+
+        return d3
+          .drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended);
+      }
     },
   },
 });
